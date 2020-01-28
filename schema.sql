842291a58c1db465b4243829c93d0d64bd50f174
Nick Koch <marauder30@gmail.com>
2020-01-27 20:56:49 -0700

    reorganizing schema

--- schema.sql
+++ schema.sql
@@ -22,13 +22,4 @@ CREATE TABLE department(
 name VARCHAR(30) NOT NULL
 );
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Nick", "Koch", 69, 420);
-
-INSERT INTO role (title, salary, department_id)
-VALUES ("weedlord", 420000, 420);
-
-INSERT INTO department (name)
-VALUES ("Smoken weed");
-
 SELECT * FROM employee, role, department;
