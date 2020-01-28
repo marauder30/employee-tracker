2ad48dc066d70f95722d2030ddd27f555e754dea
Nick Koch <marauder30@gmail.com>
2020-01-27 21:44:19 -0700

    commiting seeds despite the fact that they are multiplying

---
+++ seeds.sql
@@ -0,0 +1,61 @@
+USE roster;
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Mike", "Smith", 3, null);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Accountant", 100000, 12);
+
+INSERT INTO department (name)
+VALUES ("Accounting");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Ben", "Anderson", 4, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 75000, 10);
+
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Luke", "Douglass", 12, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Graphic Artist", 100000, 9);
+
+INSERT INTO department (name)
+VALUES ("Marketing");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Michelle", "Arlington", 4, 4);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Salesperson", 70000, 10);
+
+INSERT INTO department (name)
+VALUES ("Sales");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Charlie", "Dinkins", 6, null);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("IT Specialist", 100000, 4);
+
+INSERT INTO department (name)
+VALUES ("IT");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Lindsay", "Applegate", 5, 3);
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("Production", 120000, 9);
+
+INSERT INTO department (name)
+VALUES ("Marketing");
+
+
+
+
+
+
